10fe5da949a8aabd23172b0c1b7bce39f16f6d1e
U-APAC\UAI2KOR <UAI2KOR@KOR1101916.kor.apac.bosch.com>
2020-07-31 20:49:06 +0530

    added test case

--- vehicle-collect-test.cpp
+++ vehicle-collect-test.cpp
@@ -18,6 +18,28 @@ ASSERT_EQ(vip.ip.measurement, ip.measurement);
 ASSERT_EQ(vip.stat, VALID);
 }
 
+TEST(COLLECT, convert_input_data_into_storage_format)
+{ 
+	class vehicle obj;
+	struct vehicle_output vop = {0};
+	
+	struct input ip = {
+	.id = 2300,
+	.key = BATTERY_TEMP,
+	.measurement = 40
+        };
+	struct vehicle_input vip = {
+	.ip = ip,
+	.stat = VALID
+        };
+
+vop = obj.convert_input_data_into_storage_format(vip);
+ASSERT_EQ(vop.id, vip.ip.id);
+ASSERT_EQ(vop.motor_temp, 0);
+ASSERT_EQ(vop.battery_temp, 40);
+ASSERT_EQ(vop.battery_soc, 0);
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
